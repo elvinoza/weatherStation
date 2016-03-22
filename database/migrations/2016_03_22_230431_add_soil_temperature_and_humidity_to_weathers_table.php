0101deafc83afad10f18f8451fd7ece203f0e3a5
elvinoza <elvinoza@gmail.com>
2016-03-22 23:08:58 +0200

    Added soil temperature and humidity fields

---
+++ database/migrations/2016_03_22_230431_add_soil_temperature_and_humidity_to_weathers_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddSoilTemperatureAndHumidityToWeathersTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table('weathers', function($table)
+		{
+			$table->double('soil_temperature', 4, 2);
+			$table->double('soil_humidity', 5, 2);
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::table('weathers', function($table)
+		{
+			$table->dropColumn('soil_temperature');
+			$table->dropColumn('soil_humidity');
+		});
+	}
+
+}
